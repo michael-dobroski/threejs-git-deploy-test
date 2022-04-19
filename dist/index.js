001ce171cf36fe1b351b031c0fcc7b1cb3dfb0bf
michael-dobroski <michael-dobroski@outlook.com>
2022-04-18 21:10:11 -0600

    trying more

--- dist/index.js
+++ dist/index.js
@@ -18,8 +18,10 @@ function requestAccess() {
     }
   })
   .catch(console.error);
-  button.remove();
+  // button.remove();
 }
+
+document.getElementById("demo").onclick = function() {requestAccess()};
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
