0f59834035319787d7b24036ab7e17f901858b2c
michael-dobroski <michael-dobroski@outlook.com>
2022-04-18 19:14:21 -0600

    Updates

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ effect.render( scene, camera );
 
 // Loading images into VR environment
 let materialArray = [];
-let texture_ft = new THREE.TextureLoader().load( 'meadow_ft.jpg');
-let texture_bk = new THREE.TextureLoader().load( 'meadow_bk.jpg');
-let texture_up = new THREE.TextureLoader().load( 'meadow_up.jpg');
-let texture_dn = new THREE.TextureLoader().load( 'meadow_dn.jpg');
-let texture_rt = new THREE.TextureLoader().load( 'meadow_rt.jpg');
-let texture_lf = new THREE.TextureLoader().load( 'meadow_lf.jpg');
+let texture_ft = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_ft.jpg');
+let texture_bk = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_bk.jpg');
+let texture_up = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_up.jpg');
+let texture_dn = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_dn.jpg');
+let texture_rt = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_rt.jpg');
+let texture_lf = new THREE.TextureLoader().setCrossOrigin( true ).load( 'meadow_lf.jpg');
 
 //Overlapping mesh objects with textures  
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
