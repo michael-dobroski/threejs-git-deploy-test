de7145899cb3b52bbc5fae89daebe6e409aea2ec
Will Hunnius <90058793+whunnius@users.noreply.github.com>
2022-04-19 20:23:26 -0500

    Updates

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ scene.add( skybox );
 function moveCamera() {
   window.addEventListener('deviceorientation', function(event) {
     const t = document.body.getBoundingClientRect().top;
-    camera.rotation.z = event.gamma*.008;
-    camera.rotation.x = event.alpha*.008;
-    camera.rotation.y = event.beta*.008;
+    camera.rotation.z = event.alpha*.008;
+    camera.rotation.x = event.beta*.008;
+    camera.rotation.y = event.gamma*.008;
   });
   
 }
